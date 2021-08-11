619116a07f681ebbe8d56144f812a556a1a96112
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2021-08-11 15:43:55 +0200

    Updated to angular 12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })
   ],
   providers: [],
   bootstrap: [AppComponent]
